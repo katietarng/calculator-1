bdf71d69ccad5156db89095dc4f9be34c13207ee
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:14:26 -0700

    With docstrings

--- arithmetic.py
+++ arithmetic.py
@@ -1,23 +1,31 @@
 def add(num1, num2):
+    """Input two numbers and return the sum""" 
     return num1 + num2
 
 def subtract(num1, num2):
+    """Input two numbers and return the difference"""
     return num1 - num2
 
 def multiply(num1, num2):
+    """Input two numbers and return the product"""
     return num1 * num2
 
 def divide(num1, num2):
+    """Input two numbers and return the quotient"""
     return num1/num2
 
 def square(num1):
+    """Input one number and return the square"""
     return num1 * num1
 
 def cube(num1):
+    """Input one number and return the cube"""
     return num1 * num1 * num1
 
 def power(num1, num2):
+    """Input two numbers and return the num1 multiplied by num2 times"""
     return num1**num2
 
 def mod(num1, num2):
+    """Input two numbers and return the remainder"""
     return num1%num2
